a24e90382e2cc7290722693322aa06ace6c0e8a5
Conner Sommerfield <conner.sommerfield@gmail.com>
2024-02-20 01:41:44 -0800

    Update scripts

--- clean_lib.sh
+++ clean_lib.sh
@@ -1,2 +1,4 @@
+echo "Let's Start ROBOT_LIB From Scratch"
+
 shopt -s extglob
-rm -v $HOME/ROBOT_LIB/!("install.sh"|"checkout.sh"|"export.sh"|view_lib.sh|clean_lib.sh)
+rm -v $HOME/ROBOT_LIB/!("install.sh"|"checkout.sh"|"export.sh"|"see_lib.sh"|"clean_lib.sh")
--- see_lib.sh
+++ see_lib.sh
@@ -1,1 +1,8 @@
-ls $HOME/ROBOT_LIB
+echo -e "Here are the scripts available in ROBOT_LIB\n"
+echo -e "############################################"
+echo -e ":):):):):):):):):):):):):):):):):):):):):):)"
+echo -e "############################################\n"
+
+ls -1 $HOME/ROBOT_LIB
+
+echo -e "\nI <3 ROBOT_LIB!"
