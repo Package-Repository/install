bb4663f1aa713965d8c8a330d51d9997d91d1048
kchan5071 <emailogabe@gmail.com>
2024-02-23 18:57:06 -0800

    install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,8 @@ mkdir -p "$INSTALL_DIR"
 # Define the source directory and the destination directory
 destination_dir=$INSTALL_DIR
 # Use the find command to locate all Python files in the source directory and its subdirectories
-find . -type f -name "*.py" -exec cp {} "$destination_dir" \;
-find . -type f -name "*.sh" -exec cp {} "$destination_dir" \;
+find . -type f -name "*.py"   -exec cp {} "$destination_dir" \;
+find . -type f -name "*.sh"   -exec cp {} "$destination_dir" \;
 find . -type f -name "*.json" -exec cp {} "$destination_dir" \;
 
 sudo chmod +x $INSTALL_DIR/*
